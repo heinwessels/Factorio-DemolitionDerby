0dc413317e73329af01a4d0b789dd986656cc6d0
Hein Wessels <heinwessels93@gmail.com>
2021-05-29 22:58:41 +0200

    Removed tips and tricks. Hehe

--- data.lua
+++ data.lua
@@ -9,3 +9,7 @@ require("prototypes.coloured-tiles")  -- Only used when blueprinting splash
 
 -- Just make artillery a little stronger
 data.raw["artillery-projectile"]["artillery-projectile"].action.action_delivery.target_effects[1].action.radius = 5
+
+-- Remove all tips and tricks
+data.raw["tips-and-tricks-item"] = nil
+data.raw["tips-and-tricks-item-category"] = nil
