8d2dff8a465faf884a2b6f573afbc8f6bfc5ff09
Hein Wessels <heinwessels93@gmail.com>
2022-05-28 16:36:35 +0200

    Fix possible crash in collision when player character is killed. Character is now invincible!

--- data.lua
+++ data.lua
@@ -7,3 +7,37 @@ require("prototypes.sound")
 require("prototypes.tips-and-tricks")
 
 -- require("prototypes.coloured-tiles")  -- Only used when blueprinting splash
+
+
+-- Character needs to be immune to everything, because we don't
+-- want it to die by accident when flung out of it's vehicle.
+data.raw.character.character.resistances = {
+    {
+        type = "fire",
+        percent = 100       -- immune
+    },
+    {
+        type = "poison",
+        percent = 100       -- immune
+    },
+    {
+        type = "acid",
+        percent = 100       -- immune
+    },
+    {
+        type = "impact",
+        percent = 100       -- immune
+    },
+    {
+        type = "physical",
+        percent = 100       -- immune
+    },
+    {
+        type = "electric",
+        percent = 100       -- immune
+    },
+    {
+        type = "explosion",
+        percent = 100       -- immune
+    },
+}
